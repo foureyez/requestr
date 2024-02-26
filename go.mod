de33021fb87a6dab8baac659be6bac14635a6948
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-02-26 09:33:54 +0530

    Add init

--- go.mod
+++ go.mod
@@ -21,11 +21,19 @@ require (
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2
-	github.com/rivo/uniseg v0.4.6 // indirect
+	github.com/rivo/uniseg v0.4.7-0.20240127222946-601bbb3750c2 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/term v0.9.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
 
-require github.com/atotto/clipboard v0.1.4 // indirect
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/rivo/tview v0.0.0-20240204151237-861aa94d61c8
+)
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,81 @@
 package main
 
 import (
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/rs/zerolog/log"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
 
-	"github.com/foureyez/requestr/internal/home"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	home := home.NewHome()
-	p := tea.NewProgram(home)
-	if _, err := p.Run(); err != nil {
-		log.Fatal().Msgf("Unable to initialize app: %s", err.Error())
+	app := tview.NewApplication()
+	newPrimitive := func(text string) *tview.TextView {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(text)
 	}
+	menu := newPrimitive("Right Side")
+	main := newPrimitive("Main content")
+	sideBar := newPrimitive("Side Bar")
+
+	urlInputField := tview.NewInputField().
+		SetPlaceholder("Enter url")
+
+	urlInputField.SetDoneFunc(func(key tcell.Key) {
+		url := urlInputField.GetText()
+		res, err := makeHttpCall(url)
+		if err != nil {
+			main.SetText(err.Error())
+		} else {
+			main.SetText(fmt.Sprintf("%+v", res))
+		}
+	})
+
+	grid := tview.NewGrid().
+		SetRows(3, 0).
+		SetColumns(30, 0, 30).
+		SetBorders(true).
+		AddItem(urlInputField, 0, 0, 1, 3, 1, 0, true)
+
+	grid.SetBordersColor(tcell.Color44)
+
+	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
+	grid.AddItem(menu, 0, 0, 0, 0, 0, 0, false).
+		AddItem(main, 1, 0, 1, 3, 0, 0, false).
+		AddItem(sideBar, 0, 0, 0, 0, 0, 0, false)
+
+	// Layout for screens wider than 100 cells.
+	grid.AddItem(menu, 1, 0, 1, 1, 0, 100, false).
+		AddItem(main, 1, 1, 1, 1, 0, 100, false).
+		AddItem(sideBar, 1, 2, 1, 1, 0, 100, false)
+
+	if err := app.SetRoot(grid, true).Run(); err != nil {
+		log.Fatal().Msgf("Unable to start application: %s", err.Error())
+	}
+}
+
+func makeHttpCall(url string) (*httpRes, error) {
+	c := &http.Client{Timeout: 10 * time.Second}
+	res, err := c.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	body, _ := io.ReadAll(res.Body)
+	return &httpRes{
+		statusCode: res.StatusCode,
+		status:     res.Status,
+		body:       body,
+		headers:    res.Header,
+	}, nil
+}
+
+type httpRes struct {
+	headers    map[string][]string
+	status     string
+	body       []byte
+	statusCode int
 }
